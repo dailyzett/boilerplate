a0123488300fd516846aa4aaf8a8fe24d756a56c
김세준 <dailyzett@gmail.com>
2024-08-03 13:24:29 +0900

    feat: 사용자 정보 DB로부터 불러오기 기능 구현

--- boiler-docs/src/main/kotlin/org/example/boilerdocs/controller/DbUserController.kt
+++ boiler-docs/src/main/kotlin/org/example/boilerdocs/controller/DbUserController.kt
@@ -1,11 +1,11 @@
 package org.example.boilerdocs.controller
 
+import org.example.boilerdocs.dto.UserReq
 import org.example.boilerdocs.entity.TUser
 import org.example.boilerdocs.repo.UserRepository
-import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,9 +14,9 @@ import org.springframework.web.bind.annotation.RestController
 class DbUserController(
     private val userRepository: UserRepository,
 ) {
-    @GetMapping("/{id}")
-    fun userDetails(@PathVariable id: Int): ResponseEntity<TUser> {
-        val foundUser: TUser? = userRepository.findByIdOrNull(id)
+    @PostMapping
+    fun userDetails(@RequestBody req: UserReq): ResponseEntity<TUser> {
+        val foundUser: TUser? = userRepository.findTUserByNameAndHobby(req.name, req.hobby)
         return ResponseEntity.ok(foundUser)
     }
 }
--- boiler-docs/src/main/kotlin/org/example/boilerdocs/repo/UserRepository.kt
+++ boiler-docs/src/main/kotlin/org/example/boilerdocs/repo/UserRepository.kt
@@ -3,4 +3,6 @@ package org.example.boilerdocs.repo
 import org.example.boilerdocs.entity.TUser
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository : JpaRepository<TUser, Int>
+interface UserRepository : JpaRepository<TUser, Int> {
+    fun findTUserByNameAndHobby(name: String, hobby: String): TUser?
+}
